9652f6babf0d4c08695be1b12706a11d9bf53929
fekar0507 <55133638+fekar0507@users.noreply.github.com>
2019-12-03 23:58:44 +0900

    로그인관련
    로그인 정보쓰기->로그인하면 검색페이지로/로그아웃기능/로그인기능(데베에서 끌어와서 대조 등)

---
+++ LOGIN_PAGE.php
@@ -0,0 +1,72 @@
+<?php
+                $connect = mysqli_connect("127.0.0.1","root","54321","LoginTest");                
+
+ 
+                @session_start();
+		
+ 
+                if(isset($_SESSION['ID'])) {	
+                        echo $_SESSION['NICKNAME'];?>님 안녕하세요
+                        <br/>
+			<input type="button" value="LOGOUT" onclick='location.replace("./logout_action.php")'>
+			
+			<br></br>
+	<form name="searchmovie" action="search_movie.php" method="post">
+		<select name="category">
+
+                	<option value="TITLE">TITLE</option>
+
+                	<option value="ACTOR">ACTOR</option>
+            	</select> 
+	<input type="text" size="50" name="SEARCHTERM">
+	<input type="button" name="SEARCH" value="Search" onclick="mysub(1)">
+	</form>
+
+        <?php
+                }
+                else {
+        ?>              
+			<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"> 
+<title>Join Us</title>
+
+</head>
+<body>
+<form name="login" method="post" action='login_action.php'>
+ <h1>LOGIN</h1>
+ <table border="1">
+  <tr>
+   <td>ID</td>
+   <td><input type="text" size="30" name="ID"></td>
+  </tr>
+  <tr>
+   <td>PASSWORD</td>
+   <td><input type="password" size="30" name="PASSWORD"></td>
+  </tr>
+   </table>
+ <input type="button" value="INSERT" onclick="mysub(2)"><input type=reset value="REWRITE">
+</form>
+
+                        <br />
+        <?php   }
+        ?>
+
+<script>
+
+	function mysub(index){
+		if(index==1){
+			document.searchmovie.action="search_movie.php";
+			document.searchmovie.submit();
+		}
+		if(index==2){									document.login.action="login_action.php";
+			document.login.submit();
+		}	
+	}
+
+</script>
+
+
+</body>
+</html>
