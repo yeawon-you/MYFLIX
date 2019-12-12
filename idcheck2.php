68b38b82cc1ed92b9043d2332ab023bc0019507f
fekar0507 <55133638+fekar0507@users.noreply.github.com>
2019-12-12 22:03:52 +0900

    Update idcheck2.php
    echo를 alert로 수정함

--- idcheck2.php
+++ idcheck2.php
@@ -14,16 +14,32 @@ if($connect)
     
     //ID을 입력하지 않았을 때 
     if($ID == ''){
-        echo 'ID는 4글자부터 가능합니다.';
+?>
+	<script>
+                alert("ID는 4글자부터 가능합니다.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+
+<?php
 	exit;
     }
 
     //길이제한 ID(4~15자) 
     if(mb_strlen($ID)<4){
-        echo 'ID는 4~15자 이내로 가능합니다.';
+?>
+	<script>
+                alert("ID는 4~15자 이내로 가능합니다.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+<?php
 	exit;
     }else if(15<mb_strlen($ID)){
-        echo 'ID는 4~15자 이내로 가능합니다.';
+?>
+	<script>
+                alert("ID는 4~15자 이내로 가능합니다.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+<?php
 	exit;
     }
 
@@ -32,11 +48,21 @@ if($connect)
     $sql = "SELECT * FROM Users WHERE ID = '{$ID}'"; //Users테이블의 ID 칼럼에서 입력된 ID와 같은 값을 가진 데이터를 가져오는 명령어
     $res = mysqli_query($connect,$sql); //명령어 데이터베이스에서 실행하여 res에 값저장 
     if($res->num_rows >= 1){ //res가 1줄이상이면 이미 존재
-        echo '이미 존재하는 아이디가 있습니다.';
+?>
+        <script>
+                alert("이미 존재하는 아이디 입니다.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+<?php
         exit;
     }	
     else{
-    	echo '사용 가능한 아이디 입니다.';
+?>
+	<script>
+                alert("사용가능한 아이디 입니다.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+<?php
     }
 
 
