6ea2cd1ae92eea5a286a0b484ffeb954d9726bd7
fekar0507 <55133638+fekar0507@users.noreply.github.com>
2019-12-12 22:06:43 +0900

    Update memberSave5.php
    가입시 프로필 사진이 5개중 랜덤으로 배정되는 기능이 추가되었습니다.

--- memberSave5.php
+++ memberSave5.php
@@ -1,3 +1,4 @@
+
 <?php
     $connect = mysqli_connect("127.0.0.1","root","54321","LoginTest");
  
@@ -5,11 +6,17 @@
   //    echo "db connected";
     // else
       //echo "db not connected"; 
-	
+    
+ 
 
     $ID=$_POST['ID'];
     $PASSWORD=$_POST['PASSWORD'];
     $NICKNAME=$_POST['NICKNAME'];
+
+ 
+
+
+
     //PHP에서 유효성 재확인
  
     //아이디 중복검사.
@@ -19,33 +26,72 @@
  
     //ID, PASSWORD, NICKNAME을 입력하지 않았을 때 
     if($ID == ''){
-        echo 'ID를 입력해주세요.';
+?>
+        <script>
+                alert("아이디를 입력해주세요.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+<?php
 	exit;
     }
     else if($PASSWORD == ''){
-	echo '비밀번호를 입력해주세요.';
+?>
+	<script>
+                alert("비밀번호를 입력해주세요.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+<?php
 	exit;
     }
     else if($NICKNAME == ''){
-	echo '닉네임을 입력해주세요.';
+?>
+	<script>
+                alert("닉네임을 입력해주세요.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+
+<?php
 	exit;
     }
    
     //길이제한 ID(4~15자) , PASSWORD(8~12자), NICKNAME(2~8자)  
     if(mb_strlen($ID)<4){
-        echo 'ID는 4~15자 이내로 가능합니다.';
+?>
+	<script>
+                alert("ID는 4~15자 이내로 가능합니다.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+
+<?php
 	exit;
     }else if(15<mb_strlen($ID)){
-        echo 'ID는 4~15자 이내로 가능합니다.';
+?>
+	<script>
+                alert("ID는 4~15자 이내로 가능합니다.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+
+<?php
 	exit;
     }
 
     if(mb_strlen($PASSWORD)<8){
-        echo '비밀번호는 8~12자 이내로 가능합니다.';
+?>
+	<script>
+                alert("비밀번호는 8~12자 이내로 가능합니다.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+<?php
 	exit;
     }
     else if(12<mb_strlen($PASSWORD)){
-        echo '비밀번호는 8~12자 이내로 가능합니다.';
+?>
+	<script>
+                alert("비밀번호는 8~12자 이내로 가능합니다.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+
+<?php
 	exit;
     }else{ // 비밀번호 조건이 만족되었음을 확인하면 암호화진행 
 	$PASSWORD=md5($PASSWORD);	
@@ -53,11 +99,23 @@
 
 
     if(mb_strlen($NICKNAME)<2){
-        echo '닉네임은 2~8자 이내로 가능합니다.';
+?>
+	<script>
+                alert("닉네임은 2~8자 이내로 가능합니다.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+
+<?php
 	exit;
     }
     else if(8<mb_strlen($NICKNAME)){
-	echo '닉네임은 2~8자 이내로 가능합니다.';
+?>
+	<script>
+                alert("닉네임은 2~8자 이내로 가능합니다.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+
+<?php
 	exit;
     }
 
@@ -65,7 +123,12 @@
     $sql = "SELECT * FROM Users WHERE ID = '{$ID}'"; //Users테이블의 ID 칼럼에서 입력된 ID와 같은 값을 가진 데이터를 가져오는 명령어
     $res = mysqli_query($connect,$sql); //명령어 데이터베이스에서 실행하여 res에 값저장 
     if($res->num_rows >= 1){ //res가 1줄이상이면 이미 존재
-        echo '이미 존재하는 아이디가 있습니다.';
+?>
+        <script>
+                alert("이미 존재하는 아이디 입니다.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+<?php
         exit;
     }
 
@@ -73,24 +136,59 @@
     $sql = "SELECT * FROM Users WHERE NICKNAME = '{$NICKNAME}'";
     $res = mysqli_query($connect,$sql);
     if($res->num_rows >= 1){
-        echo '이미 존재하는 닉네임이 있습니다.';
+?>
+	<script>
+                alert("이미 존재하는 닉네임입니다.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+
+<?php
         exit;
     }
 
+    $PROFILE1='https://github.com/yeawon-you/MYFLIX/blob/master/images/profile1.png?raw=true';
+    $PROFILE2='https://github.com/yeawon-you/MYFLIX/blob/master/images/profile2.png?raw=true';
+    $PROFILE3='https://github.com/yeawon-you/MYFLIX/blob/master/images/profile3.png?raw=true';
+    $PROFILE4='https://github.com/yeawon-you/MYFLIX/blob/master/images/profile4.png?raw=true';
+    $PROFILE5='https://github.com/yeawon-you/MYFLIX/blob/master/images/profile5.png?raw=true';	
 
+
+    $randomNum=mt_rand(1,5);   
     
+    if($randomNum==1)
+	$PROFILEIMAGE=$PROFILE1;
+    else if($randomNum==2)
+	$PROFILEIMAGE=$PROFILE2;
+    else if($randomNum==3)
+	$PROFILEIMAGE=$PROFILE3;
+    else if($randomNum==4)
+	$PROFILEIMAGE=$PROFILE4;
+    else if($randomNum==5)
+	$PROFILEIMAGE=$PROFILE5;
+
 
     //위의 조건을 모두 만족함 -> 데이터베이스에 넣기 시작 
-    $sql = "INSERT INTO Users(ID, PASSWORD, NICKNAME) VALUES('{$ID}','{$PASSWORD}','{$NICKNAME}');";
+    $sql = "INSERT INTO Users(ID, PASSWORD, NICKNAME, PROFILEIMAGE) VALUES('{$ID}','{$PASSWORD}','{$NICKNAME}','{$PROFILEIMAGE}');";
  
 
 $result = mysqli_query($connect,$sql);
 
 
     if(!$result){
-        echo '오류 발생';
-    }else
-	echo '회원 가입이 완료되었습니다.';
+?>
+        <script>
+                alert("오류가 발생하였습니다. 다시 시도해주세요.");
+                location.replace("./JOINUS_FINAL.php");
+        </script>
+
+<?php
+    }else{
+?>
+	<script>
+                alert("회원가입이 완료되었습니다.");
+                location.replace("./LOGIN_PAGE.php");
+        </script>
+<?php
+    }
 
 ?>
- 
