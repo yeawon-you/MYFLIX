56ee459d9fe948e73682e593fe3f2325163bc2a2
fekar0507 <55133638+fekar0507@users.noreply.github.com>
2019-12-15 02:15:41 +0900

    Update review_list.php
    검색기능 및 페이지 넘기기기능 수정중

--- review_list.php
+++ review_list.php
@@ -1,3 +1,4 @@
+
 <?php
 	$connect = mysqli_connect("127.0.0.1","root","54321","LoginTest");
  
@@ -6,19 +7,21 @@
 
 
 //게시판 목록보기에 필요한 각종 변수 초기값을 설정합니다.
+
+$page=$_GET['page'];
+
 $tablename="Review"; //테이블 이름
-if($page == "") $page = 1; //페이지 번호가 없으면 1
+if($page == '') $page = 1; //페이지 번호가 없으면 1
 $list_num = 10; //한 페이지에 보여줄 목록 갯수
 $page_num = 10; //한 화면에 보여줄 페이지 링크(묶음) 갯수
 $offset = $list_num*($page-1); //한 페이지의 시작 글 번호(listnum 수만큼 나누었을 때 시작하는 글의 번호)
-$num=0;
 
 //전체 글 수를 구합니다. (쿼리문을 사용하여 결과를 배열로 저장하는 일반적 인 방법)
 $query="select COUNT(*) from $tablename"; // SQL 쿼리문을 문자열 변수에 일단 저장하고
 $result=mysqli_query($connect,$query); // 위의 쿼리문을 실제로 실행하여 결과를 result에 대입
-$row=mysqli_fetch_assoc($result); //위 결과 값을 하나하나 배열로 저장합니다 .
-$total_no=$row['COUNT']; //배열의 첫번째 요소의 값, 즉 테이블의 전체 글 수를 저장합니다.
- 
+$row=mysqli_fetch_row($result);
+$total_no=$row[0]; //배열의 첫번째 요소의 값, 즉 테이블의 전체 글 수를 저장합니다.
+
 //전체 페이지 수와 현재 글 번호를 구합니다.
 $total_page=ceil($total_no/$list_num); // 전체글수를 페이지당글수로 나눈 값의 올림 값을 구합니다.
 $cur_num=$total_no - $list_num*($page-1); //현재 글번호
@@ -59,14 +62,17 @@ A:hover {color:gray;text-decoration:none;}
 </tr>
  
 <?php
-while($array=mysqli_fetch_assoc($result)){
+while($array=mysqli_fetch_array($result)){
  
+
+
 $date=date("Y/m/d", $array['DATE']);
  
+
 echo '';?>
 <tr>
 <td width=30>
-<p align=center><?php echo $num;?></p>
+<p align=center><?php echo $cur_num;?></p>
 </td>
 <td width=490>
 <a href='view.php?page=$page&COUNT=<?php echo $array['COUNT']?>'><?php echo $array['TITLE'];?></a>
@@ -80,7 +86,8 @@ echo '';?>
 </tr> 
 
 <?php
-$num++;
+$cur_num--;
+
  
 }
 ?>
@@ -90,6 +97,9 @@ $num++;
 <?php
 //여기서부터 각종 페이지 링크
 //먼저, 한 화면에 보이는 블록($page_num 기본값 이상일 때 블록으로 나뉘어짐 )
+
+
+
 $total_block=ceil($total_page/$page_num);
 $block=ceil($page/$page_num); //현재 블록
  
@@ -104,14 +114,14 @@ echo "&nbsp; <p align=center>";
 //[처음][*개앞]
 if($block > 1) {
 $prev=$first-1;
-echo "<a href='list.php?page=1'>[처음 ]</a>&nbsp; ";
-echo "<a href='list.php?page=$prev'>[$page_num 개 앞]</a>";
+echo "<a href='review_list.php?page=1'>[처음 ]</a>&nbsp; ";
+echo "<a href='review_list.php?page=$prev'>[$page_num 개 앞]</a>";
 }
  
 //[이전]
 if($page > 1) {
 $go_page=$page-1;
-echo " <a href='list.php?page=$go_page'>[이전 ]</a>&nbsp; ";
+echo " <a href='review_list.php?page=$go_page'>[이전 ]</a>&nbsp; ";
 }
  
 //페이지 링크
@@ -120,21 +130,21 @@ if($page_link==$page) {
 echo "<font color=green><b>$page_link</b></font>";
 }
 else {
-echo "<a href='list.php?page=$page_link'>[$page_link]</a>";
+echo "<a href='review_list.php?page=$page_link'>[$page_link]</a>";
 }
 }
  
 //[다음]
 if($total_page > $page) {
 $go_page=$page+1;
-echo "&nbsp;<a href='list.php?page=$go_page'>[다음]</a>";
+echo "&nbsp;<a href='review_list.php?page=$go_page'>[다음]</a>";
 }
  
 //[*개뒤][마지막]
 if($block < $total_block) {
 $next=$last+1;
-echo "<a href='list.php?page=$netxt'>[$page_num 개 뒤]</a>&nbsp;";
-echo "<a href='list.php?page=$total_page'>[마지막]</a></p>";
+echo "<a href='review_list.php?page=$next'>[$page_num 개 뒤]</a>&nbsp;";
+echo "<a href='review_list.php?page=$total_page'>[마지막]</a></p>";
 }
  
 ?>
@@ -147,7 +157,7 @@ echo "<a href='list.php?page=$total_page'>[마지막]</a></p>";
 </tr>
 </table>
 
-<form name="searchreview" action="search_review.php" method="post">
+<form name="searchreview" action="search_review.php" method="get">
 		<select name="category2">
 
                 	<option value="TITLE">제목</option>
