138a4d7e236470ee7895ae4cbbf5b17b8a133855
fekar0507 <55133638+fekar0507@users.noreply.github.com>
2019-12-15 02:20:00 +0900

    Update search_review.php
    검색기능 페이지넘기기 기능 수정중

--- search_review.php
+++ search_review.php
@@ -4,29 +4,44 @@
 
     @session_start(); 
 
-$category = $_POST['category2'];
-$search_term = $_POST['SEARCHTERM2'];
+$page=$_GET['page'];
 
 //게시판 목록보기에 필요한 각종 변수 초기값을 설정합니다.
 $tablename="Review"; //테이블 이름
-if($page == "") $page = 1; //페이지 번호가 없으면 1
+
+if(!$page || $page<1) $page = 1; //페이지 번호가 없으면 1
+
+if($page == 1){
+$search_term = $_GET['SEARCHTERM2'];
+$category = $_GET['category2'];
+$status = $category;
+}
+else{
+$search_term = $_GET['SEARCHTERM2'];
+$category = $_GET['category2'];
+$status = $category;
+}
+
+
+echo $search_term;
+echo $category;
+
 $list_num = 10; //한 페이지에 보여줄 목록 갯수
 $page_num = 10; //한 화면에 보여줄 페이지 링크(묶음) 갯수
 $offset = $list_num*($page-1); //한 페이지의 시작 글 번호(listnum 수만큼 나누었을 때 시작하는 글의 번호)
-$num=0;
 
 //전체 글 수를 구합니다. (쿼리문을 사용하여 결과를 배열로 저장하는 일반적 인 방법)
 $query="select COUNT(*) from $tablename WHERE $category LIKE '%$search_term%'"; // SQL 쿼리문을 문자열 변수에 일단 저장하고
 $result=mysqli_query($connect,$query); // 위의 쿼리문을 실제로 실행하여 결과를 result에 대입
-$row=mysqli_fetch_assoc($result); //위 결과 값을 하나하나 배열로 저장합니다 .
-$total_no=$row['COUNT']; //배열의 첫번째 요소의 값, 즉 테이블의 전체 글 수를 저장합니다.
+$row=mysqli_fetch_row($result); //위 결과 값을 하나하나 배열로 저장합니다 .
+$total_no=$row[0]; //배열의 첫번째 요소의 값, 즉 테이블의 전체 글 수를 저장합니다.
  
 //전체 페이지 수와 현재 글 번호를 구합니다.
 $total_page=ceil($total_no/$list_num); // 전체글수를 페이지당글수로 나눈 값의 올림 값을 구합니다.
 $cur_num=$total_no - $list_num*($page-1); //현재 글번호
  
 //Review테이블에서 목록을 가져옵니다. (위의 쿼리문 사용예와 비슷합니다 .)
-$query="select * from $tablename WHERE $category LIKE '%$search_term%'"; // SQL 쿼리문
+$query = "SELECT * FROM $tablename WHERE $category LIKE '%$search_term%' order by COUNT desc limit $offset, $list_num"; // SQL 쿼리문
 $result=mysqli_query($connect,$query); // 쿼리문을 실행 결과
 //쿼리 결과를 하나씩 불러와 실제 HTML에 나타내는 것은 HTML 문 중간에 삽입합니다.
 ?>
@@ -61,7 +76,8 @@ A:hover {color:gray;text-decoration:none;}
 </tr>
  
 <?php
-while($array=mysqli_fetch_assoc($result)){
+
+while($array=mysqli_fetch_array($result)){
  
 $date=date("Y/m/d", $array['DATE']);
  
@@ -101,19 +117,24 @@ $last=$block*$page_num; //페이지 블록의 끝 페이지
 if($block >= $total_block) {
 $last=$total_page;
 }
+
+
  
 echo "&nbsp; <p align=center>";
 //[처음][*개앞]
+
+
+
 if($block > 1) {
 $prev=$first-1;
-echo "<a href='list.php?page=1'>[처음 ]</a>&nbsp; ";
-echo "<a href='list.php?page=$prev'>[$page_num 개 앞]</a>";
+echo "<a href='search_review.php?page=1'>[처음 ]</a>&nbsp; ";
+echo "<a href='search_review.php?page=$prev'>[$page_num 개 앞]</a>";
 }
  
 //[이전]
 if($page > 1) {
 $go_page=$page-1;
-echo " <a href='list.php?page=$go_page'>[이전 ]</a>&nbsp; ";
+echo " <a href='search_review.php?page=$go_page'>[이전 ]</a>&nbsp; ";
 }
  
 //페이지 링크
@@ -122,21 +143,21 @@ if($page_link==$page) {
 echo "<font color=green><b>$page_link</b></font>";
 }
 else {
-echo "<a href='list.php?page=$page_link'>[$page_link]</a>";
+echo "<a href='search_review.php?page=$page_link?SEARCHTERM2=$search_term?category2=$category'>[$page_link]</a>";
 }
 }
  
 //[다음]
 if($total_page > $page) {
 $go_page=$page+1;
-echo "&nbsp;<a href='list.php?page=$go_page'>[다음]</a>";
+echo "&nbsp;<a href='search_review.php?page=$go_page?SEARCHTERM2=$search_term?category2=$category'>[다음]</a>";
 }
  
 //[*개뒤][마지막]
 if($block < $total_block) {
 $next=$last+1;
-echo "<a href='list.php?page=$netxt'>[$page_num 개 뒤]</a>&nbsp;";
-echo "<a href='list.php?page=$total_page'>[마지막]</a></p>";
+echo "<a href='search_review.php?page=$next'>[$page_num 개 뒤]</a>&nbsp;";
+echo "<a href='search_review.php?page=$total_page'>[마지막]</a></p>";
 }
  
 ?>
@@ -149,16 +170,17 @@ echo "<a href='list.php?page=$total_page'>[마지막]</a></p>";
 </tr>
 </table>
 
-<form name="searchreview" action="search_review.php" method="post">
+
+<form name="searchreview" action="search_review.php" method="get">
 		<select name="category2">
 
-                	<option value="TITLE">제목</option>
+                	<option value="TITLE"<?php if($status == "TITLE") echo"SELECTED";?>>제목</option>
 
-                	<option value="WRITER">닉네임</option>
+                	<option value="WRITER"<?php if($status == "WRITER") echo"SELECTED";?>>닉네임</option>
 
-			<option value="MOVIETITLE">영화제목</option>
+			<option value="MOVIETITLE"<?php if($status == "MOVIETITLE") echo"SELECTED";?>>영화제목</option>
             	</select> 
-	<input type="text" size="50" name="SEARCHTERM2">
+	<input type="text" size="50" value="<?php echo $search_term;?>"name="SEARCHTERM2">
 	<input type="button" name="SEARCH" value="검색" onclick="button();">
 </form>
 
